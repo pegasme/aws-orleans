3c75c087ab2f791f0e9ba181101a17a9cef85fd1
Maria <pegas.pochta@gmail.com>
2025-10-12 17:06:00 +0300

    Create deploy to AWS lambda for client

--- _infra/modules/backend/main.tf
+++ _infra/modules/backend/main.tf
@@ -1,7 +1,7 @@
 
 locals {
     lambda_role_name = "${var.name}-lambda-role"
-    lambda_function_name = "${var.name}-lambda-function"
+    lambda_client_function_name = "${var.name}-client"
 
     api_gateway_name = "${var.name}-api-gateway"
 }
@@ -10,35 +10,43 @@ locals {
 # API Gateway 
 ################################
 
-resource "aws_api_gateway_rest_api" "adventure_api" {
-  name = local.api_gateway_name
+resource "aws_apigatewayv2_api" "adventure_api" {
+  name        = local.api_gateway_name
   description = "Backend API Gateway"
+  protocol_type = "HTTP"
 
-  endpoint_configuration {
-    types = ["REGIONAL"]
+  cors_configuration {
+    allow_origins = ["*"]                   # Replace with your domain for security
+    allow_methods = ["GET", "POST", "OPTIONS"]
+    allow_headers = ["content-type", "authorization"]
   }
 }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.adventure_api.id
-  parent_id = aws_api_gateway_rest_api.adventure_api.root_resource_id
-  path_part = "api"
+resource "aws_apigatewayv2_integration" "adventure_api_integration" {
+  api_id                 = aws_apigatewayv2_api.adventure_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.adventure_api.invoke_arn
+  payload_format_version = "2.0"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.adventure_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = "POST"
-  authorization = "NONE"
+resource "aws_apigatewayv2_route" "adventure_api_route" {
+  api_id    = aws_apigatewayv2_api.adventure_api.id
+  route_key = "ANY /api/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.adventure_api_integration.id}"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.adventure_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "POST"
-  type = "AWS"
-  uri = aws_lambda_function.adventure_api.invoke_arn
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.adventure_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.adventure_api.execution_arn}/*/*"
 }
 
 ################################
@@ -63,8 +71,13 @@ resource "aws_iam_role" "adventure_api_role" {
   assume_role_policy = data.aws_iam_policy_document.adventure_assume_role.json
 }
 
+resource "aws_iam_role_policy_attachment" "adventure_api_policy" {
+  role       = aws_iam_role.adventure_api_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "adventure_api" {
-    function_name    = local.lambda_role_name
+    function_name    = local.lambda_client_function_name
     package_type     = "Image"
     role             = aws_iam_role.adventure_api_role.arn
     image_uri        = var.default_image_url
