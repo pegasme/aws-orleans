7ba3a913d9de01854f2a58395a45848a3035adf5
Maria.Voronina <pegas.pochta@gmail.com>
2025-10-12 16:11:28 +0300

    Task3.fix backend cicd (#50) (#51)

--- _infra/modules/backend/variables.tf
+++ _infra/modules/backend/variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   type        = string
   description = "A name to associate with API Gateway."
 }
+
+variable "default_image_url" {
+  type        = string
+  description = "The URI of a container image in ECR."
+}
