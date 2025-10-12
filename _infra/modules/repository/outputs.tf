7f47e74e853bc85304310712c96a32bb57fc88ea
Maria <pegas.pochta@gmail.com>
2025-10-12 16:15:31 +0300

    Task3.fix backend cicd (#50)

--- _infra/modules/repository/outputs.tf
+++ _infra/modules/repository/outputs.tf
@@ -2,3 +2,8 @@ output "ecr_role_arn" {
   description = "ECR role ARN"
   value       = aws_iam_role.ecr_access_role.arn
 }
+
+output "repository_name" {
+  description = "ECR Repository name"
+  value       = aws_iam_role.aws_ecr_repository.name
+}
