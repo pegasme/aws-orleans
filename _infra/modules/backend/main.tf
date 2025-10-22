682416b4c9c559ffb92fb80fdaa541e80ca149ab
Maria <pegas.pochta@gmail.com>
2025-10-22 10:49:12 +0300

    delete private route table

--- _infra/modules/backend/main.tf
+++ _infra/modules/backend/main.tf
@@ -147,21 +147,6 @@ resource "aws_route_table_association" "route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = var.vpc_id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-  tags = { Name = "ecs-private-rt" }
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  count          = 2
-  subnet_id      = var.private_subnet_ids[count.index]
-  route_table_id = aws_route_table.private.id
-}
-
 ################
 # Security Group
 ################
