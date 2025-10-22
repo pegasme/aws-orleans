ab6ba8d6846ffd13820567142c3468559b94fc88
Maria <pegas.pochta@gmail.com>
2025-10-22 12:26:33 +0300

    create 3 security groups

--- _infra/modules/gateway/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  type        = string
-  description = "A name to associate with the gateway and its resources."
-}
-
-variable "vpc_id" {
-  description = "VPC"
-  type        = string
-}
