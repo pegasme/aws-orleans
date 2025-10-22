45050d6b8b908837d2c2c745c1a77359dd82b39d
Maria <pegas.pochta@gmail.com>
2025-10-22 11:07:14 +0300

    delete everything and start from scratch

--- _infra/modules/backend/main.tf
+++ _infra/modules/backend/main.tf
@@ -123,79 +123,17 @@ resource "aws_cloudwatch_log_group" "adventure_lambda_logging" {
 # ECS Service
 ###############
 
-resource "aws_internet_gateway" "internet_gateway" {
- vpc_id = var.vpc_id
- tags = {
-   Name = "${local.aws_ecs_service_name}-ig"
- }
+# -------------------------
+# ECS Cluster
+# -------------------------
+
+resource "aws_ecs_cluster" "adventure_cluster" {
+ name = local.aws_ecs_cluster_name
 }
 
-resource "aws_route_table" "public_route_table" {
- vpc_id = var.vpc_id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway.id
- }
- tags = {
-   Name = "${local.aws_ecs_service_name}-rt"
- }
-}
-
-resource "aws_route_table_association" "route_table_association" {
-  count          = 2
-  subnet_id      = var.public_subnet_ids[count.index]
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-################
-# Security Group
-################
-resource "aws_security_group" "adventure_server_security_group" {
- name   = "${local.aws_ecs_service_name}-sg"
- vpc_id = var.vpc_id
-
- ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-   Name = "${local.aws_ecs_service_name}-private-sg"
- }
-}
-
-# Lambda SG to access ECS
-resource "aws_security_group" "lambda_sg" {
-  name   = "lambda-sg"
-  vpc_id = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.adventure_server_security_group.id]
-  }
-}
-
-###############
+# -------------------------
 # IAM Role
-################
+# -------------------------
 
 resource "aws_iam_role" "adventure_ecs_node_role" {
   name_prefix        = "${local.aws_ecs_service_name}-role"
@@ -218,231 +156,6 @@ resource "aws_iam_role_policy_attachment" "ecs_node_role_policy" {
 resource "aws_iam_instance_profile" "adventure_ecs_node" {
   name_prefix = "${local.aws_ecs_service_name}-profile"
   role        = aws_iam_role.adventure_ecs_node_role.name
-}
-
-# -------------------------
-# ECS Cluster
-# -------------------------
-
-resource "aws_ecs_cluster" "adventure_cluster" {
- name = local.aws_ecs_cluster_name
-}
-
-# -------------------------
-# Launch Template
-# -------------------------
-
-resource "aws_launch_template" "adventure_server_ecs_lt" {
- name_prefix   = "${local.aws_ecs_service_name}-tmpl"
- image_id      = data.aws_ssm_parameter.ecs_node_ami.value
- instance_type = "t3.micro"
- key_name      = local.aws_ecs_keyapir
-
- vpc_security_group_ids = [aws_security_group.adventure_server_security_group.id]
- 
- iam_instance_profile { 
-    name = aws_iam_instance_profile.adventure_ecs_node.name 
- }
-
- tag_specifications {
-   resource_type = "instance"
-   tags = {
-     Name = "ecs-instance"
-   }
- }
-
- user_data = base64encode(<<-EOF
-      #!/bin/bash
-      echo ECS_CLUSTER=${aws_ecs_cluster.adventure_cluster.name} >> /etc/ecs/ecs.config;
-
-      # Install CloudWatch Agent
-        yum install -y amazon-cloudwatch-agent
-        cat <<CWAGENT > /opt/aws/amazon-cloudwatch-agent/bin/config.json
-        {
-          "metrics": {
-            "append_dimensions": {
-              "AutoScalingGroupName": "$${aws:AutoScalingGroupName}",
-              "InstanceId": "$${aws:InstanceId}"
-            },
-            "metrics_collected": {
-              "mem": {
-                "measurement": ["mem_used_percent"]
-              },
-              "disk": {
-                "measurement": ["disk_used_percent"],
-                "resources": ["*"]
-              }
-            }
-          }
-          CWAGENT
-    systemctl enable amazon-cloudwatch-agent
-    systemctl start amazon-cloudwatch-agent
-    EOF
-      )
-}
-
-# -------------------------
-# Auto Scaling Group
-# -------------------------
-
-resource "aws_autoscaling_group" "adventure_server_ecs_asg" {
- name                      = "${local.aws_ecs_service_name}-asg"
- vpc_zone_identifier       = var.private_subnet_ids
- desired_capacity          = 1
- max_size                  = 2
- min_size                  = 1
- 
- lifecycle {
-    create_before_destroy = true
-  }
-
- launch_template {
-   id      = aws_launch_template.adventure_server_ecs_lt.id
-   version = "$Latest"
- }
-
- tag {
-   key                 = "AmazonECSManaged"
-   value               = true
-   propagate_at_launch = true
- }
-}
-
-# -------------------------
-# ECS Capacity Provider
-# -------------------------
-
-resource "aws_ecs_capacity_provider" "adventure_ecs_capacity_provider" {
- name = "${local.aws_ecs_service_name}-cp"
-
- auto_scaling_group_provider {
-   auto_scaling_group_arn = aws_autoscaling_group.adventure_server_ecs_asg.arn
-
-   managed_scaling {
-     maximum_scaling_step_size = 2
-     minimum_scaling_step_size = 1
-     status                    = "ENABLED"
-     target_capacity           = 80
-   }
- }
-}
-
-resource "aws_ecs_cluster_capacity_providers" "adventure_capacity_providers" {
- cluster_name = aws_ecs_cluster.adventure_cluster.name
-
- capacity_providers = [aws_ecs_capacity_provider.adventure_ecs_capacity_provider.name]
-
- default_capacity_provider_strategy {
-   base              = 1
-   weight            = 1
-   capacity_provider = aws_ecs_capacity_provider.adventure_ecs_capacity_provider.name
- }
-}
-
-# ------------------------------------------------
-# ECS TASK DEFINITIONs
-# ------------------------------------------------
-
-resource "aws_ecs_task_definition" "adventure_server_task_definition" {
-  family             = local.aws_task_def_name
-  cpu                = 256
-  memory             = "512"
-  network_mode       = "bridge"
-  execution_role_arn = "arn:aws:iam::${local.account_id}:role/ecsTaskExecutionRole"
-
-  runtime_platform {
-   operating_system_family = "LINUX"
-   cpu_architecture        = "X86_64"
-  }
-
-  container_definitions = jsonencode([{
-    name      = local.aws_ecs_container_name
-    image     = var.default_server_image_url
-    essential = true
-    memory    = 512
-    portMappings = [
-      { "containerPort": 80, "hostPort": 80 }
-    ],
-    environment = [
-      { name  = "ENVIRONMENT", value = "Production" },
-      { name  = "ASPNETCORE_ENVIRONMENT", value = "Production" },
-      { "name": "ORLEANS_CLUSTER_ID", "value": "ecs-orleans-cluster" },
-      { "name": "ORLEANS_SERVICE_ID", "value": "ecs-orleans-service" },
-      { "name": "AWS_REGION", "value": "us-east-1" }
-    ]
-  }])
-}
-
-resource "aws_ecs_service" "adventure-server" {
-  name            = local.aws_ecs_service_name
-  desired_count   = 1
-  task_definition = aws_ecs_task_definition.adventure_server_task_definition.arn
-  cluster         = aws_ecs_cluster.adventure_cluster.id
-  launch_type     = "EC2"
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.adventure_ecs_tg.arn
-    container_name   = local.aws_ecs_container_name
-    container_port   = 80
-  }
-
-  depends_on = [aws_lb_listener.adventure_ecs_listener]
-
-  tags = {
-   name = local.server_tag_name
- }
-}
-
-# ------------------------------------------------
-# Load balancer 
-# ------------------------------------------------
-
-resource "aws_security_group" "alb" {
-  vpc_id = var.vpc_id
-  name   = "alb-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { Name = "alb-sg" }
-}
-
-resource "aws_lb" "adventure_ecs_lb" {
-  name               = "adventure-ecs-lb"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.adventure_server_security_group.id]
-  subnets            = var.public_subnet_ids
-}
-
-resource "aws_lb_target_group" "adventure_ecs_tg" {
-  name     = "adventure-ecs-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "instance"
-}
-
-resource "aws_lb_listener" "adventure_ecs_listener" {
-  load_balancer_arn = aws_lb.adventure_ecs_lb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.adventure_ecs_tg.arn
-  }
 }
 
 # ----------------------------------------------------
@@ -506,8 +219,7 @@ resource "aws_iam_policy" "ecs_orleans_dynamodb_policy" {
           "dynamodb:DeleteItem",
           "dynamodb:Scan",
           "dynamodb:Query",
-          "dynamodb:DescribeTable",
-          "dynamodb:CreateTable"
+          "dynamodb:DescribeTable"
         ],
         Resource = [
           var.dynamodb_table_arn,
@@ -522,7 +234,3 @@ resource "aws_iam_role_policy_attachment" "ecs_attach_orleans_dynamodb" {
   role       = aws_iam_role.adventure_ecs_node_role.name
   policy_arn = aws_iam_policy.ecs_orleans_dynamodb_policy.arn
 }
-
-
-
-
